8244b57ab5f2524cc4863a85665fd444572d382f
minseok1109 <56787096+minseok1109@users.noreply.github.com>
2023-11-03 16:14:20 +0000

    Update page.tsx

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -2,21 +2,27 @@ type Props = {};
 const Publications = (props: Props) => {
   return (
     <div className={'flex flex-col gap-4 mb-8'}>
-      <ul className="flex flex-col gap-4 my-8">
+      <ul className="flex flex-col gap-4 my-4">
+        <li>
+          <b>Kim, S.</b>, & Oh, J. (In press). A Study on secondary reader's
+          reading experience survey – Focusing on perceptions of negative
+          reading experience. The Korean Journal of Literature & Language
+          Education, 45. [In Korean]
+        </li>
         <li>
           <b>Kim, S.</b> (2023). Information acceptance and perspective
-          reconstruction study in multiple-text reading – Based on secondary
+          reconstruction study in multiple-text reading – based on secondary
           reader's stance. Reading Research, 68, 181-216. [In Korean]
         </li>
         <li>
-          <b>Kim, S.</b> & Kim, Y. (2023). A Study on the effect of writer's
+          <b>Kim, S.</b> & Kim, Y. (2023). A study on the effect of writer's
           characteristics on writing performance in relation to the Topic.
           Writing Research, 58, 07-43. [In Korean]
         </li>
         <li>
-          <b> Kim, S.</b> (2023). Analysis of differences in writing and
+          <b>Kim, S.</b> (2023). Analysis of differences in writing and
           motivation based on topic selection in middle school student's
-          expository writing. The Korean journal of Literacy Research, 14(1),
+          expository writing. The Korean Journal of Literacy Research, 14(1),
           143-168. [In Korean]
         </li>
         <li>
@@ -25,27 +31,33 @@ const Publications = (props: Props) => {
           Reading Research, 65, 81-109. [In Korean]
         </li>
       </ul>
-      <span className="my-4 font-bold">(In press)</span>
-      <ul className="flex flex-col gap-4 my-4">
-        <li>
-          <b>Kim, S.</b>,& Lee, Y., Forzani, E. (2023). Development and Validation of a
-          Self-Efficacy Assessment Questionnaire for Adolescent Readers’ On-line
-          Reading Ability. [In English]
-        </li>
-      </ul>
+
       <span className="font-bold">(In preparation – Finished Writing)</span>
       <ul className="flex flex-col gap-4 mt-6">
         <li>
-          <b>Kim, S.</b>,& Lee, Y., E (2023). Development and Validation of a
-          Self-Efficacy Assessment Questionnaire for Adolescent Readers’ On-line
-          Reading Ability. [In English]
+          <b>Kim, S.</b>, Lee, Y. & Forzani, E. (2023). Development and
+          validation of a self-efficacy assessment questionnaire for adolescent
+          readers’ on-line reading ability. [In English]
         </li>
       </ul>
       <ul className="flex flex-col gap-4 my-2">
         <li>
-          Oh, J., &<b>Kim, S.</b>(2023). A Study on secondary reader's
-          self-selected reading. The Korean journal of Literature & Language
+          Oh, J., & <b>Kim, S.</b> (2023). A study on secondary reader's
+          self-selected reading. The Korean Journal of Literature & Language
           Education. [In Korean]
+        </li>
+      </ul>
+      <span className="my-4 font-bold">(In revision)</span>
+      <ul className="flex flex-col gap-4 my-2">
+        <li>
+          <b>Kim, S.</b>, & Han, I. (in revision). Learners’ credibility
+          assessment in Online texts and the factors influencing it.
+          Learner-centered Curriculum and Instruction. [In English]
+        </li>
+        <li>
+          <b>Kim, S.</b>, & Oh, S. (in revision). Secondary students’
+          self-evaluation in multiple-text learning and performance. The Korean
+          Journal of Literacy Research. [In Korean]
         </li>
       </ul>
     </div>
